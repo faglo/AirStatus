d6c018666cb839126aeb30a79486e70c8018c5a2
Faglo <bkom01@icloud.com>
2020-08-26 02:06:31 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# **AirStatus**
+#### Check your AirPods battery level from tray in Windows
+
+#### What is it?
+This is Python 3.6 script that allows you to check AirPods battery level **just by looking on icon** or get percents **by clicking _right button_** on icon
+
+#### Can I customize it easily?
+**Yes, you can!**
+
+You can customize icons and replace it with certain names or change **update frequency** and **low level warning percent** in start of main.py file
+
+#### Why percents are displayed in dozens?
+This is Apple restrictions, only genius or Apple can fix that :(
+
+#### Used materials
+* Some code from [this repo](https://github.com/ohanedan/Airpods-Windows-Service)
+* Battery icons from [Icons8](https://icons8.com/icon/set/battery/windows)
+
+#### TODO
+* Improve icons
+* Display more info
+* Fix bugs
+* Port to Linux
---
+++ main.py
@@ -0,0 +1,244 @@
+from bleak import discover
+from PIL import Image
+from asyncio import new_event_loop, set_event_loop, get_event_loop
+from pystray import Icon, Menu, MenuItem as Item
+from multiprocessing import Process
+from time import sleep
+from win10toast import ToastNotifier
+
+# Configure update duration (update after n seconds)
+UPDATE_DURATION = 30
+# Configure battery level when you get toast notification of discharging
+LOW_LEVEL = 20
+
+
+# Getting data with hex format
+async def get_device():
+    # Scanning for devices
+    devices = await discover()
+    for d in devices:
+        # Checking for AirPods
+        if d.rssi >= -690 and 76 in d.metadata['manufacturer_data'] and len(
+                d.metadata['manufacturer_data'][76].hex()) == 54:
+            data = d.metadata['manufacturer_data'][76].hex()
+            return data
+        else:
+            return False
+
+
+# Same as get_device() but it's standalone method instead of async
+def get_data_hex():
+    new_loop = new_event_loop()
+    set_event_loop(new_loop)
+    loop = get_event_loop()
+    a = loop.run_until_complete(get_device())
+    return a
+
+
+# Getting data from hex string and converting it to dict(json)
+def get_data():
+    raw = get_data_hex()
+
+    # Return blank data if airpods not found
+    if not raw:
+        return dict(status=0, model="AirPods not found")
+
+    # On 7th position we can get AirPods model, Pro or standart
+    if raw[7] == 'e':
+        model = " Pro"
+    else:
+        model = ""
+
+    # Checking left AirPod for availability and storing charge in variable
+    try:
+        left = int(raw[12]) * 10
+    except ValueError:
+        left = -1
+
+    # Checking right AirPod for availability and storing charge in variable
+    try:
+        right = int(raw[13]) * 10
+    except ValueError:
+        right = -1
+
+    # Checking AirPods case for availability and storing charge in variable
+    try:
+        case = int(raw[15]) * 10
+    except ValueError:
+        case = -1
+
+    # On 14th position we can get charge status of AirPods, I found it with some tests :)
+    charge_raw = raw[14]
+    if charge_raw == "a":
+        charging = "one"
+    elif charge_raw == "b":
+        charging = "both"
+    else:
+        charging = "N/A"
+
+    # Return result info in dict format
+    return dict(
+        status=1,
+        charge=dict(
+            left=left,
+            right=right,
+            case=case
+        ),
+        charging=charging,
+        model="AirPods"+model
+    )
+
+
+# Checking AirPods availability and create icon in tray
+def create_icon(status, left, right, case, model):
+    # Rewriting data dict because cant pass all dict with multiprocessing lib
+    data = dict(
+        status=status,
+        charge=dict(
+            left=left,
+            right=right,
+            case=case
+        ),
+        model=model
+    )
+    # Blank values
+    a_left = True
+    a_right = True
+    a_case = True
+    charges = dict(
+        left=-1,
+        right=-1,
+        case=-1
+    )
+
+    if data["status"] == 0:
+        # Setting false availability for all devices and setting icon path
+        a_left = False
+        a_right = False
+        a_case = False
+        image = "./icons/no.png"
+    else:
+        # Checking for availability and errors for connected devices
+        charges = data["charge"]
+        if charges["left"] == -1 or charges["left"] == 150:
+            a_left = False
+        if charges["right"] == -1 or charges["right"] == 150:
+            a_right = False
+        if charges["case"] == -1:
+            a_case = False
+
+    # Right click menu
+    menu = Menu(
+        Item(
+            text=data["model"],
+            action="",
+            enabled=False
+        ),
+        Item(
+            text="Left: {}%".format(charges["left"]),
+            action="",
+            enabled=False,
+            visible=a_left
+        ),
+        Item(
+            text="Right: {}%".format(charges["right"]),
+            action="",
+            enabled=False,
+            visible=a_right
+        ),
+        Item(
+            text="Case: {}%".format(charges["case"]),
+            action="",
+            enabled=False,
+            visible=a_case
+        )
+    )
+
+    # Selecting lowest charge level for comparing and icon select
+    if a_left and charges["left"] > charges["right"]:
+        lowest = charges["left"]
+    elif a_right and charges["right"] > charges["left"]:
+        lowest = charges["right"]
+    elif charges["right"] == charges["left"]:
+        lowest = charges["right"]
+    else:
+        lowest = -1
+
+    # Selecting icon for charge levels
+    if lowest == -1:
+        image = "./icons/no.png"
+    elif lowest < 20:
+        image = "./icons/empty.png"
+    elif lowest < 40:
+        image = "./icons/low.png"
+    elif lowest < 60:
+        image = "./icons/middle.png"
+    elif lowest < 80:
+        image = "./icons/much.png"
+    elif lowest < 100:
+        image = "./icons/full.png"
+    else:
+        image = "./icons/no.png"
+
+    # Creating icon
+    Icon(data["model"], Image.open(image), menu=menu).run()
+
+
+# Simple method for notification show
+def low_level_notification(model, percent):
+    notifer = ToastNotifier()
+    notifer.show_toast(model, "Your battery is going low ({}%)".format(percent), "./icons/low_n.ico", 5, True)
+
+
+def run():
+    data = get_data()
+    connected = True
+    cache = None
+    cached_process = None
+
+    while True:
+        if data["status"] == 1:
+            # Checking cache and current data for avoid Windows duplicate tray icon bug
+            if cache != data["charge"] or not connected:
+                # Flushing process(tray icon) and handling error that might be on start
+                try:
+                    cached_process.terminate()
+                except AttributeError:
+                    pass
+
+                # Starting new thread(process)
+                proc = Process(target=create_icon, args=(1,
+                                                         data["charge"]["left"],
+                                                         data["charge"]["right"],
+                                                         data["charge"]["case"],
+                                                         data["model"]))
+                proc.start()
+
+                # Setting cache vars
+                cached_process = proc
+                cache = data["charge"]
+
+                # Checking for low level for notify show
+                if int(data["charge"]["left"]) <= LOW_LEVEL or int(data["charge"]["right"]) <= LOW_LEVEL:
+                    low_level_notification(data["model"], data["charge"]["right"])
+
+        elif data["status"] == 0:
+            # Checking cache and current data for avoid Windows duplicate tray icon bug
+            if connected:
+                # Flushing process(tray icon) and handling error that might be on start
+                try:
+                    cached_process.terminate()
+                except AttributeError:
+                    pass
+                # Creating process and setting cache var
+                proc = Process(target=create_icon, args=(0, -1, -1, -1, data["model"]))
+                connected = False
+                proc.start()
+
+        sleep(UPDATE_DURATION)
+
+
+if __name__ == '__main__':
+    run()
+
+
